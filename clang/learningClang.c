18a9781a7a22ece70e5d0c63d55b15919a168dd3
Rahul Ramjeawon <96823012+CodeWithTejpratab@users.noreply.github.com>
2023-11-28 18:47:02 -0500

    Update learningClang.c

--- clang/learningClang.c
+++ clang/learningClang.c
@@ -5,16 +5,19 @@ char name[20];
 int age;
 float gpa;
 
+//Taking a String input
 printf("Enter your name: ");
 scanf("%s", name);
 
-
+//Interger Input
 printf("Enter your age: ");
 scanf("%d", &age);
 
+//Taking a Float point Number Input
 printf("What is your GPA: ");
 scanf("%f", &gpa);
 
+//Printing and formating information
 printf("Hello %s, you are %d years old, and your GPA is %0.2f\n", name, age, gpa);
 }
 
