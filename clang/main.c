05b45e1948851be8a71c7eda37f8180d601b5632
Rahul Ramjeawon <96823012+CodeWithTejpratab@users.noreply.github.com>
2023-12-14 23:13:17 -0500

    Add files via upload

---
+++ clang/main.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+//compling c code 
+//gcc -o objectfilename sourcefilename.c
+
+void checkScore (float UserInput, int* countLess, int* countMore){
+    if (UserInput <= 50){
+        *countLess+=1;
+    }
+    else if (UserInput >= 90){
+        *countMore+=1;
+    }
+}
+
+void min(int a, int b, int* smallest){
+    if(a<b){
+        *smallest = a;
+    }
+    else{
+        *smallest = b;
+    }
+}
+
+void swap(int* a, int* b){
+	*a = *a+*b;
+	*b = *a-*b;
+	*a = *a-*b;
+}
+
+void printArray(int* array, int size){
+	for (int i = 0; i < size; i++){
+		printf("intdex %d is %d", i, *(array+i));
+	}
+}
+
+void invertarray (int* myarray, int length){
+    int lastIndex = length;
+    for (int i  = 0; i < length/2; i++){
+            swap((myarray+i),(myarray+(lastIndex-1)));
+            lastIndex--;
+	} 
+}
+
+int main() {
+	char name[20]; 
+    printf("Hello World!\n");
+    printf("What is your name: ");
+	scanf("%s", name);
+	printf("Your name is %s\n", name);
+
+	int x;
+	printf("Enter a number: ");
+	scanf("%x", &x);
+	printf("%d, then %o, then %x\n", x, x,x );
+	return 0;
+}
+	
